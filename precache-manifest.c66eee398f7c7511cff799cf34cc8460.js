9e84d4b312c5fb49dd419d47c74369f53d2d1ab1
200801 <Hsy123>
2021-07-29 08:46:01 +0800

    deploy

---
+++ precache-manifest.c66eee398f7c7511cff799cf34cc8460.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe5f7b6c46283e8fab8a",
+    "url": "/Tally-book-website/css/app.c8e9f8e0.css"
+  },
+  {
+    "revision": "5d39cdc7306bdd709716c92b81cb799f",
+    "url": "/Tally-book-website/index.html"
+  },
+  {
+    "revision": "fe5f7b6c46283e8fab8a",
+    "url": "/Tally-book-website/js/app.6c7f262a.js"
+  },
+  {
+    "revision": "e8563cf97abdf3de7d5c",
+    "url": "/Tally-book-website/js/chunk-vendors.5b841680.js"
+  },
+  {
+    "revision": "d04864db5217486b6293c4dfb4ae3dc1",
+    "url": "/Tally-book-website/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/Tally-book-website/robots.txt"
+  }
+]);
